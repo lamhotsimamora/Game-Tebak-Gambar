e88f597ef1bb77c3fdc98783a1527d5bdba322f9
Lamhot Simamora <lamhotsimamora36@gmail.com>
2020-05-20 00:06:53 +0700

    _

--- init.js
+++ init.js
@@ -1,75 +1,75 @@
 let card_collection = [
     {
-        image : 'image/card_javascript.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_javascript.png',
         id : 'card_javascript',
         index : null
     },
     {
-        image : 'image/card_php.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_php.png',
         id : 'card_php',
         index : null
     },
     {
-        image : 'image/card_python.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_python.png',
         id : 'card_python',
         index : null
     },
     {
-        image : 'image/card_cplus.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_cplus.png',
         id : 'card_cplusplus',
         index : null
     }
     ,
     {
-        image : 'image/card_java.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_java.png',
         id : 'card_java',
         index : null
     }
     ,
     {
-        image : 'image/card_ruby.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_ruby.png',
         id : 'card_ruby',
         index : null
     }
     ,
     {
-        image : 'image/card_golang.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_golang.png',
         id : 'card_golang',
         index : null
     }
     ,
     {
-        image : 'image/card_csharp.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_csharp.png',
         id : 'card_csharp',
         index : null
     }
     ,
     {
-        image : 'image/card_flutter.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_flutter.png',
         id : 'card_flutter',
         index : null
     }
     ,
     {
-        image : 'image/card_kotlin.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_kotlin.png',
         id : 'card_kotlin',
         index : null
     }
     ,
     {
-        image : 'image/card_swift.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_swift.png',
         id : 'card_swift',
         index : null
     }
     ,
     {
-        image : 'image/card_vue.png',
+        image : 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_vue.png',
         id : 'card_vue',
         index : null
     }
 ]
 
-const card_default = '/image/card_default.png';
+const card_default = 'https://lamhotsimamora.github.io/Game-Tebak-Gambar/image/card_default.png';
 
 const click_sound = document.getElementById("click_sound"); 
 const match_sound = document.getElementById("match_sound"); 
